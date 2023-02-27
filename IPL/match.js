8ecba697dbd731d51457d5e879769bac31fc393b
KHS21 <126454548+KHS21@users.noreply.github.com>
2023-02-27 13:57:57 +0530

    Add files via upload

---
+++ IPL/match.js
@@ -0,0 +1,92 @@
+<script>
+    var teamlistDatafunction =[
+
+    {
+        "id" : "1",
+        "logo": "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/Royal_Challengers_Bangalore_2020.svg/1200px-Royal_Challengers_Bangalore_2020.svg.png",
+        "NAME": "Royal Challengers Bengaluru - (RCB)",
+        "CHAMPIONSHIP WON": "0",
+        "NO. OF PLAYERS": "11"
+    },
+
+    {
+        
+        "id" : "2",
+        "logo": "MI",
+        "NAME": "MUMBAI INDIANS - (MI)",
+        "CHAMPIONSHIP WON": "5",
+        "NO. OF PLAYERS": "11",
+
+    },
+
+    {
+        
+        "id" : "3", 
+        "logo": "CSK",
+        "NAME": "Chennai Super Kings - (CSK)",
+        "CHAMPIONSHIP WON": "3",
+        "NO. OF PLAYERS": "11"
+
+    }
+
+    {
+        
+        "id" : 
+        "logo":
+        "NAME":
+        "CHAMPIONSHIP WON":
+        "NO. OF PLAYERS":
+
+    }
+
+    {
+        
+        "id" : 
+        "logo":
+        "NAME":
+        "CHAMPIONSHIP WON":
+        "NO. OF PLAYERS":
+
+    }
+
+    {
+        
+        "id" : 
+        "logo":
+        "NAME":
+        "CHAMPIONSHIP WON":
+        "NO. OF PLAYERS":
+
+    }
+
+    {
+        
+        "id" : 
+        "logo":
+        "NAME":
+        "CHAMPIONSHIP WON":
+        "NO. OF PLAYERS":
+
+    }
+
+    {
+        
+        "id" : 
+        "logo":
+        "NAME":
+        "CHAMPIONSHIP WON":
+        "NO. OF PLAYERS":
+
+    }
+] 
+
+
+
+
+
+createlistCard(pos){
+    var item = videoPlaySectionData.filter(function(val,id){
+        return val.id == pos
+    })
+}
+</script>
